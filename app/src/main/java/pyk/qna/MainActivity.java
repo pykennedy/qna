6ac89a1508b0c3bb766b5c462d0f2b6f5f75f6f0
Peter Kennedy <pykennedy@gmail.com>
2017-12-17 15:28:02 -0800

    poc works

--- app/src/main/java/pyk/qna/MainActivity.java
+++ app/src/main/java/pyk/qna/MainActivity.java
@@ -1,13 +1,44 @@
 package pyk.qna;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import eightbitlab.com.blurview.BlurView;
+import eightbitlab.com.blurview.RenderScriptBlur;
 
 public class MainActivity extends AppCompatActivity {
+  FrameLayout frameLayout;
+  View purple;
+  BlurView blurView;
+  View red;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    frameLayout = (FrameLayout) findViewById(R.id.root);
+    purple = (View) findViewById(R.id.purple);
+    blurView = (BlurView) findViewById(R.id.blurView);
+    red = (View) findViewById(R.id.red);
+    
+    setupBlurView();
+  }
+  
+  private void setupBlurView() {
+    final float radius = 25f;
+    final float minBlurRadius = 10f;
+    final float step = 4f;
+    
+    //set background, if your root layout doesn't have one
+    final Drawable windowBackground = getWindow().getDecorView().getBackground();
+    
+    final BlurView.ControllerSettings topViewSettings = blurView.setupWith(frameLayout)
+                                                                   .windowBackground(windowBackground)
+                                                                   .blurAlgorithm(new RenderScriptBlur(this))
+                                                                   .blurRadius(radius);
   }
 }
