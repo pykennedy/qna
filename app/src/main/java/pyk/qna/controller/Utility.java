6ec68c30f52ac63e47de57c09d55ca54f2332f65
Peter Kennedy <pykennedy@gmail.com>
2018-03-11 19:31:11 -0700

    users can edit their profile

--- app/src/main/java/pyk/qna/controller/Utility.java
+++ app/src/main/java/pyk/qna/controller/Utility.java
@@ -1,7 +1,112 @@
 package pyk.qna.controller;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Base64;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import pyk.qna.App;
+
 public class Utility {
-  public static String cleanEmail (String email) {
-    return email.replace('@','a').replace('.','d');
+  public static String cleanEmail(String email) {
+    return email.replace('@', 'a').replace('.', 'd');
+  }
+  
+  public static Bitmap getBitmapFromDrawable(int resourceID) {
+    return BitmapFactory.decodeResource(App.get().getResources(), resourceID);
+  }
+  
+  public static Bitmap scaleDownBitmap(Bitmap bm) {
+    final int MAX_WIDTH  = 250;
+    final int MAX_HEIGHT = 250;
+    int       width      = bm.getWidth();
+    int       height     = bm.getHeight();
+    
+    if (width > height) {
+      // landscape
+      float ratio = (float) width / MAX_WIDTH;
+      width = MAX_WIDTH;
+      height = (int) (height / ratio);
+    } else if (height > width) {
+      // portrait
+      float ratio = (float) height / MAX_HEIGHT;
+      height = MAX_HEIGHT;
+      width = (int) (width / ratio);
+    } else {
+      // square
+      height = MAX_HEIGHT;
+      width = MAX_WIDTH;
+    }
+    
+    bm = Bitmap.createScaledBitmap(bm, width, height, true);
+    return bm;
+  }
+  
+  public static String bitmapToBase64(Bitmap bitmap) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+    byte[] b            = baos.toByteArray();
+    String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+    return encodedImage;
+  }
+  
+  public static Bitmap base64ToBitmap(String base64) {
+    byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
+    Bitmap decodedByte   = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+    return decodedByte;
+  }
+  
+  public static Bitmap rotateBitmapIfNeeded(Uri uri, Bitmap bitmap) {
+    Bitmap adjustedBitmap = bitmap;
+    try {
+      Cursor cursor = App.get().getContentResolver().query(uri, null, null, null, null);
+      cursor.moveToFirst();
+      int           idx  = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+      ExifInterface exif = new ExifInterface(cursor.getString(idx));
+      int rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                          ExifInterface.ORIENTATION_NORMAL);
+      int rotationDegrees = 0;
+      if (rotation == ExifInterface.ORIENTATION_ROTATE_90) {
+        rotationDegrees = 90;
+      } else if (rotation == ExifInterface.ORIENTATION_ROTATE_180) {
+        rotationDegrees = 90;
+      } else if (rotation == ExifInterface.ORIENTATION_ROTATE_270) {
+        rotationDegrees = 90;
+      }
+      
+      Matrix matrix = new Matrix();
+      if (rotation != 0f) {
+        matrix.preRotate(rotationDegrees);
+      }
+      adjustedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                                           bitmap.getHeight(), matrix, true);
+      
+    } catch (IOException e) {
+      Toast.makeText(App.get(), "Load image failed, try again.", Toast.LENGTH_SHORT).show();
+      e.printStackTrace();
+    }
+    return adjustedBitmap;
+  }
+  
+  public static void handlePermission(Activity activity) {
+    if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(activity,
+                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        0);
+    }
   }
 }
---
+++ app/src/main/java/pyk/qna/controller/fragment/EditProfileDialog.java
@@ -0,0 +1,139 @@
+package pyk.qna.controller.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import pyk.qna.App;
+import pyk.qna.R;
+import pyk.qna.controller.Utility;
+import pyk.qna.model.firebase.FirebaseHandler;
+import pyk.qna.model.object.User;
+
+import static android.app.Activity.RESULT_OK;
+
+// TODO: clicking on the username signs out the user and opens the login dialog
+public class EditProfileDialog extends DialogFragment
+    implements View.OnClickListener, FirebaseHandler.Delegate {
+  User user = null;
+  private Bitmap imageBM;
+  TextView        username;
+  CircleImageView circleImageView;
+  EditText        description;
+  ProgressBar     progressBar;
+  
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    Dialog              dialog;
+    AlertDialog.Builder builder    = new AlertDialog.Builder(getActivity());
+    LayoutInflater      inflater   = getActivity().getLayoutInflater();
+    View                dialogView = inflater.inflate(R.layout.dialog_edit_profile, null);
+    builder.setView(dialogView);
+    dialog = builder.create();
+    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    
+    progressBar = dialogView.findViewById(R.id.login_pb);
+    username = dialogView.findViewById(R.id.tv_edit_username);
+    circleImageView = dialogView.findViewById(R.id.civ_edit_image);
+    description = dialogView.findViewById(R.id.et_edit_description);
+    description.setImeOptions(EditorInfo.IME_ACTION_DONE);
+    description.setRawInputType(InputType.TYPE_CLASS_TEXT);
+    
+    circleImageView.setOnClickListener(this);
+    
+    FirebaseHandler fb = FirebaseHandler.getFb();
+    fb.setDelegate(this);
+    fb.readUser(fb.getCurrentUsername());
+    
+    return dialog;
+  }
+  
+  @Override public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.civ_edit_image:
+        Intent pickIntent = new Intent(Intent.ACTION_PICK,
+                                       android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+        startActivityForResult(pickIntent, 1);
+        break;
+      default:
+        break;
+    }
+  }
+  
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode == RESULT_OK) {
+      Uri imageUri = data.getData();
+      if (imageUri.toString().contains("image")) {
+        try {
+          imageBM = MediaStore.Images.Media.getBitmap(App.get().getContentResolver(),
+                                                      imageUri);
+          imageBM = Utility.scaleDownBitmap(imageBM);
+          
+          imageBM = Utility.rotateBitmapIfNeeded(imageUri, imageBM);
+          circleImageView.setImageBitmap(imageBM);
+        } catch (IOException e) {
+          Toast.makeText(App.get(), "Load image failed, try again.", Toast.LENGTH_SHORT).show();
+        }
+        
+      } else if (imageUri.toString().contains("video")) {
+        Toast.makeText(App.get(), "You must select an image", Toast.LENGTH_SHORT).show();
+      }
+    }
+  }
+  
+  @Override
+  public void onDismiss(final DialogInterface dialogInterface) {
+    if (user != null) {
+      user.setPhoto(Utility.bitmapToBase64(((BitmapDrawable)circleImageView.getDrawable()).getBitmap()));
+      user.setDescription(description.getText().toString());
+      FirebaseHandler fb = FirebaseHandler.getFb();
+      fb.writeUser(user);
+    }
+    super.onDismiss(dialogInterface);
+  }
+  
+  @Override public void onLoginSuccess(String successType) {}
+  
+  @Override public void onLoginFailed(String errorType)    {}
+  
+  @Override public void onReadUserSuccess(User user) {
+    this.user = user;
+    progressBar.setVisibility(View.GONE);
+    username.setVisibility(View.VISIBLE);
+    circleImageView.setVisibility(View.VISIBLE);
+    description.setVisibility(View.VISIBLE);
+    
+    username.setText(user.getUsername());
+    circleImageView.setImageBitmap(
+        (user.getPhoto() == null) ? Utility.getBitmapFromDrawable(R.drawable.emptyimage)
+                                  : Utility.base64ToBitmap(user.getPhoto()));
+    description.setText(user.getDescription());
+  }
+  
+  @Override public void onReadQuestionSuccess(List<String> result) {}
+  
+  @Override public void onReadAnswerSuccess(List<String> result)   {}
+}
